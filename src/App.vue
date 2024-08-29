20b30d0f2cf270040fe296b7f8c2fb0621959cc1
Juandre Nimb <64599836+NimbJuandre@users.noreply.github.com>
2024-08-29 21:08:43 +0200

    Added the dialog functionality

--- src/App.vue
+++ src/App.vue
@@ -1,104 +1,72 @@
 <template>
   <div ref="globeContainer" class="globe-container"></div>
+  <v-fab-transition>
+    <v-fab v-if="globeLoaded" class="fab" color="primary" icon="mdi-plus" location="bottom right" size="40" absolute
+      offset @click="dialog = !dialog"></v-fab>
+  </v-fab-transition>
+  <v-dialog v-model="dialog" max-width="500">
+    <v-card>
+      <v-card-text>
+        <v-text-field v-model="author" label="Author"></v-text-field>
+        <v-text-field v-model="quote" counter max="100" label="Quote"></v-text-field>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="primary" variant="text" @click="submit()">
+          Submit
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 <script setup>
 import { ref, onMounted } from 'vue';
-import tippy from 'tippy.js';
-import 'tippy.js/dist/tippy.css'; // optional for styling
 
 const globeContainer = ref(null);
+const globeLoaded = ref(false);
+const dialog = ref(false);
+const author = ref();
+const quote = ref();
+var globe = null;
+var pointsData = [];
+const position = ref({});
 
 onMounted(() => {
-  const globe = Globe()(globeContainer.value)
+  globe = Globe()(globeContainer.value)
     .width(window.innerWidth)
     .height(window.innerHeight)
     .backgroundColor('#000')
     .globeImageUrl('//unpkg.com/three-globe/example/img/earth-night.jpg')
     .bumpImageUrl('//unpkg.com/three-globe/example/img/earth-topology.png');
 
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((position) => {
-      const { latitude, longitude } = position.coords;
+  globeLoaded.value = true;
 
-      globe.pointsData([{ lat: latitude, lng: longitude, id: 1 }]);
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((pos) => {
+      position.value = pos.coords;
+      globe
+        //.pointsData([{ lat: latitude, lng: longitude, id: 1 }])
+        .pointAltitude(0)
+        .pointRadius(0.2)        
+        .pointLabel(p => `${p.author}: ${p.quote}`)
+        .pointOfView({ lat: position.value.latitude, lng: position.value.longitude }, 0);
 
       globe.onPointClick((point) => {
-        tippy(globeContainer.value, {
-          content: `Location: (${point.lat}, ${point.lng})`,
-          placement: 'right',
-          trigger: 'manual',
-        }).show();
+
       });
     });
   } else {
     alert('Geolocation is not supported by your browser.');
   }
 });
-</script>
 
-
-<!-- <template>
-  <div id="globeViz"></div>
-  <v-card class="quote-card ma-5" max-width="500" position="absolute" elevation="5">
-    <v-card-title>Add a quote</v-card-title>
-    <v-card-text>
-      <v-form>
-        <v-textarea class="quote-text" label="Label"></v-textarea>
-      </v-form>
-    </v-card-text>
-
-    <v-card-actions>
-      <v-btn text="Add"></v-btn>
-    </v-card-actions>
-  </v-card>
-</template>
-<script setup>
-import { ref, onMounted } from "vue"
-
-const globeViz = ref(null);
-const location = ref(null);
-const error = ref(null);
-
-onMounted(() => {
-  getLocation()
-  setupGlobe()
-});
-
-const setupGlobe = async () => {
-  const weightColor = d3.scaleSequentialSqrt(d3.interpolateYlOrRd)
-    .domain([0, 1e7]);
-
-  const world = Globe()
-    (document.getElementById('globeViz'))
-    .globeImageUrl('//unpkg.com/three-globe/example/img/earth-night.jpg')
-    .bumpImageUrl('//unpkg.com/three-globe/example/img/earth-topology.png')
-    .backgroundImageUrl('//unpkg.com/three-globe/example/img/night-sky.png')
-    .hexAltitude(d => d.sumWeight * 6e-8)
-    .hexBinResolution(4)
-    .hexBinMerge(true)
-    .enablePointerInteraction(false); // performance improvement
-
-  fetch('/src/assets/world_population.csv').then(res => res.text())
-    .then(csv => d3.csvParse(csv, ({ lat, lng, pop }) => ({ lat: +lat, lng: +lng, pop: +pop })))
-    .then(data => world.hexBinPointsData(data));
+const submit = () => {
+  pointsData.push({ lat: position.value.latitude, lng: position.value.longitude, author: author.value, quote: quote.value, id: 1 });
+  globe.pointsData(pointsData);
+  dialog.value = false;
+  author.value = "";
+  quote.value = "";
 }
-const getLocation = () => {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        location.value = {
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude
-        };
-      },
-      (err) => {
-        error.value = "Error: " + err.message;
-      }
-    );
-  } else {
-    error.value = "Geolocation is not supported by this browser.";
-  }
-};
 </script>
 <style>
 #app {
@@ -110,6 +78,11 @@ const getLocation = () => {
   padding: 0px;
 }
 
+.fab {
+  bottom: 35px;
+  right: 10px;
+}
+
 .quote-card {
   top: 1px
 }
@@ -117,4 +90,4 @@ const getLocation = () => {
 .quote-text textarea {
   height: 80px;
 }
-</style> -->
+</style> 
--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import { createApp } from 'vue'
 // Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
 
 import './style.css'
 import App from './App.vue'
@@ -13,6 +15,13 @@ const app = createApp(App);
 const vuetify = createVuetify({
     components,
     directives,
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+            mdi,
+        },
+    },
 })
 
 createApp(App).use(vuetify).mount('#app')
